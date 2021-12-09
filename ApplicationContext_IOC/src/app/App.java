adbe655d57c4566db49b8cf4d8421bdd3373b09e
Shivam Kumar <shivamkumargupta07@gmail.com>
2021-12-09 22:06:40 +0530

    Adding DependencyInjectionExamples

---
+++ ApplicationContext_IOC/src/app/App.java
@@ -0,0 +1,15 @@
+package app;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("/resources/springApp.xml");
+		
+	}
+}
---
+++ ApplicationContext_IOC/src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+	int rollNo;
+	String name;
+	public Test() {
+		System.out.println("Test Constructor Called...");
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+}
