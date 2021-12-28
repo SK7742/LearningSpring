07a1127fc74ea9162bfd0b7c4b667603b850be27
Shivam Kumar <shivamkumargupta07@gmail.com>
2021-12-28 15:25:05 +0530

    BeanLifeCycleByAnnotationApproach

---
+++ BeanLifeCycleByAnnotationApproach/src/beans/Test.java
@@ -0,0 +1,62 @@
+package beans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Test{
+
+	private String driver,url,username,password;
+	public Connection con = null;
+	public String getDriver() {
+		return driver;
+	}
+
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@PostConstruct
+	public void connectionInit() throws Exception{
+		System.out.println("Executing afterPropertiesSet (Init) Method");
+		Class.forName(driver);
+	    con = DriverManager.getConnection(url, username, password);
+		System.out.println(con.toString());
+	}
+	
+	@PreDestroy
+	public void connectionCleanup() throws Exception {
+		System.out.println("Executing destroy Method");
+		con.close();
+	}
+}
