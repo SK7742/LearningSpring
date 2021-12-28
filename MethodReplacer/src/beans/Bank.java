686c6ea3921e37b4d44aa7d201d523897064d2ac
Shivam Kumar <shivamkumargupta07@gmail.com>
2021-12-28 18:10:04 +0530

    MethodReplacer

---
+++ MethodReplacer/src/beans/Bank.java
@@ -0,0 +1,17 @@
+package beans;
+
+public class Bank {
+	public void deposite() {
+		System.out.println("Deposite method called...");
+	}
+	public void withdraw() {
+		System.out.println("withdraw method called...");
+	}
+	
+	//suppose we don't want use this defination for this method for some time then,
+	//it can be replaced by method replacer beans.NewInterestCalculation
+	//this method will override using proxy class generated at runtime.
+	public void interestRate() {
+		System.out.println("interestRate method called...");
+	}
+}
