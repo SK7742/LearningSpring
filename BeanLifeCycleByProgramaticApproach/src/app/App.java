60417523d0d3963eb2643b2336797ce507d26317
Shivam Kumar <shivamkumargupta07@gmail.com>
2022-01-08 11:19:49 +0530

    SpringMvc-Hello World Example

--- BeanLifeCycleByProgramaticApproach/src/app/App.java
+++ BeanLifeCycleByProgramaticApproach/src/app/App.java
@@ -5,7 +5,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
-
 	public static void main(String[] args) throws Exception {
 		
 		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("resources/spring.xml");
